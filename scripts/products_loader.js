15d4f1b1d02a3801b9f0e4db8837792376722b36
Force! <maxfunmaxfun672@gmail.com>
2024-04-18 23:07:03 +0300

    asa

--- scripts/products_loader.js
+++ scripts/products_loader.js
@@ -79,7 +79,7 @@ function updatePage(page, prevProductsPerPage) {
         productElement.classList.add(type);
         productElement.innerHTML = `
             <a href="${apath}">
-                <img src="${imgpath}">
+                <img src="${imagesFolder + imgpath}">
             </a>
             <div class="name-and-price">
                 <div class="name">${name}</div>
