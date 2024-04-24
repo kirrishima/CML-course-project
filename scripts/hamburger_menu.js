d45c901288721ced5985f30a42d348986d854f6a
Force! <maxfunmaxfun672@gmail.com>
2024-04-25 00:30:13 +0300

    renamed the classes in header

--- scripts/hamburger_menu.js
+++ scripts/hamburger_menu.js
@@ -21,34 +21,33 @@ function ToggleMenu() {
     let body = document.querySelector("body");
     body.classList.toggle("menu-open");
 
-    let navBar = document.querySelector(".nav-bar");
+    let navBar = document.querySelector(".menu");
     if (navBar.classList.contains("active")) {
-        navBar.classList.toggle("active");
+        // navBar.classList.toggle("active");
         navBar.style.animation =
             "slideOut 0.2s cubic-bezier(0.42, 0, 0.58, 1) forwards";
         body.style.overflow = "auto";
         toggleOriginal();
     } else {
-        navBar.classList.toggle("active");
+        
         navBar.style.display = "flex"; // Показываем блок
         navBar.style.animation = "slideIn 0.2s cubic-bezier(0.42, 0, 0.58, 1) forwards";
         // Отключаем прокрутку страницы
         body.style.overflow = "hidden";
         toggleCross();
     }
-
+navBar.classList.toggle("active");
     let navElement = document.querySelector("div.navigation");
     let navHeight = navElement.offsetHeight;
-    let navBarActive = document.querySelector(".nav-bar");
-    navBarActive.style.top = navHeight + "px";
+    navBar.style.top = navHeight + "px";
 }
-document.querySelector(".box").addEventListener("click", ToggleMenu);
+document.querySelector(".hamburger-menu").addEventListener("click", ToggleMenu);
 function disableMenu() {
-    if (document.querySelector(".nav-bar").classList.contains("active")) {
+    if (document.querySelector(".menu").classList.contains("active")) {
         ToggleMenu();
     }
 }
-document.querySelectorAll(".nav-items").forEach((navItem) => {
+document.querySelectorAll(".menu-item").forEach((navItem) => {
     navItem.addEventListener("click", function () {
         disableMenu();
     });
@@ -56,5 +55,6 @@ document.querySelectorAll(".nav-items").forEach((navItem) => {
 window.addEventListener('resize', () => {
     if (window.innerWidth > 768) {
         disableMenu();
+        document.querySelector(".menu").removeAttribute('style');
     }
 })
